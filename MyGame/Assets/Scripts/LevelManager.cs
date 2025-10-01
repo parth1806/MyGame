3c5539356cd2189788eca3ce8e119ebee3a6799c
parth1806 <makadiaparth99@gmail.com>
2025-10-01 16:04:22 +0530

    Setup different card images in the pair of two.

--- MyGame/Assets/Scripts/LevelManager.cs
+++ MyGame/Assets/Scripts/LevelManager.cs
@@ -9,6 +9,9 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GridLayoutGroup gridLayoutGroup;
     [SerializeField] private Card cardPrefab;
 
+    public Sprite[] cardSprites;
+    private int[] _shuffledCardsIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,11 @@ public class LevelManager : MonoBehaviour
         var rows = 2;
         var columns = 3;
 
+        _shuffledCardsIndex = new int[rows * columns];
+        for (var i = 0; i < _shuffledCardsIndex.Length; i++)
+        {
+            _shuffledCardsIndex[i] = i / 2; // Setup pair of cards.
+        }
 
         gridLayoutGroup.constraintCount = rows;
         for (var i = 0; i < rows; i++)
@@ -28,7 +36,9 @@ public class LevelManager : MonoBehaviour
             {
                 var cell = i * columns + j;
                 var card = Instantiate(cardPrefab, gridLayoutGroup.transform);
-
+                var cardId = _shuffledCardsIndex[cell];
+                var frontImage = cardSprites[cardId];
+                card.Setup(frontImage);
             }
         }
     }
